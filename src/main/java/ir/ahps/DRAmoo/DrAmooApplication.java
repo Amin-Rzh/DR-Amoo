60a3c07b6a0b01771666261d9c2ae7f2c247ef8c
amin.raeiszadeh@offis.de <amin.raeiszadeh@offis.de>
2020-11-18 14:47:22 +0100

    Project base is committed.

---
+++ src/main/java/ir/ahps/DRAmoo/DrAmooApplication.java
@@ -0,0 +1,13 @@
+package ir.ahps.DRAmoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrAmooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrAmooApplication.class, args);
+	}
+
+}
